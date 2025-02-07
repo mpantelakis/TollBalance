8582f8df863be29a09d0185f388adb4d7374eb5c
Anti <el20159@mail.ntua.gr>
2025-02-07 16:04:33 +0200

    Fix API Call for Debt History

--- back-end/routes/website.js
+++ back-end/routes/website.js
@@ -6,7 +6,15 @@ const {
     TotalNotVerified,
     SettleDebt,
     VerifyPayment,
-    HistoryDebt
+    HistoryDebt,
+    TrafficVariation,
+    RoadsPerOperator,
+    TrafficVariationPerRoad,
+    TrafficDistribution,
+    TollBooths,
+    DebtHistoryChart,
+    OwedAmountsChart,
+    RevenueDistribution
 } = require("../controllers/websiteControllers");
 const authenticateUser = require("../middleware/userAuthentication");
 
@@ -19,5 +27,13 @@ websiteRouter.get("/totalnotverified/:id", authenticateUser, TotalNotVerified);
 websiteRouter.post("/settledebt/:debtorId/:creditorId", authenticateUser, SettleDebt);
 websiteRouter.post("/verifypayment/:creditorId/:debtorId", authenticateUser, VerifyPayment);
 websiteRouter.get("/historydebt/:creditorId/:debtorId", authenticateUser, HistoryDebt);
+websiteRouter.get("/trafficvariation/:id/:date_from/:date_to", authenticateUser, TrafficVariation);
+websiteRouter.get("/roadsperoperator/:id", authenticateUser, RoadsPerOperator);
+websiteRouter.get("/trafficvariationperroad/:id/:road/:date_from/:date_to", authenticateUser, TrafficVariationPerRoad);
+websiteRouter.get("/trafficdistribution/:id/:date_from/:date_to", authenticateUser, TrafficDistribution);
+websiteRouter.get("/tollbooths/:id/:date_from/:date_to", authenticateUser, TollBooths);
+websiteRouter.get("/debthistorychart/:id/:date_from/:date_to", authenticateUser, DebtHistoryChart);
+websiteRouter.get("/owedamountschart/:id/:date_from/:date_to", authenticateUser, OwedAmountsChart);
+websiteRouter.get("/revenuedistribution/:id/:date_from/:date_to", authenticateUser, RevenueDistribution);
 
 module.exports = websiteRouter;
